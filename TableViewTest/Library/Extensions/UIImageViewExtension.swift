98049dd80eea0e7efca403bd9504b2dbda1b36fb
teja <tejagoud.ios@gmail.com>
2021-11-07 19:56:53 +1100

    Added Library Files

---
+++ TableViewTest/Library/Extensions/UIImageViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageViewExtension.swift
+//  TableViewTest
+//
+//  Created by TejaDanasvi on 5/11/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
