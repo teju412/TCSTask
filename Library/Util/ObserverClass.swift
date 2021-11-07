93f8d8b4730e288c9d9f62a8161558956ebc224b
teja <tejagoud.ios@gmail.com>
2021-11-07 18:25:39 +1100

    Added observer class

---
+++ Library/Util/ObserverClass.swift
@@ -0,0 +1,22 @@
+//  Observer.swift
+//  ReactivePrograming
+//  Created by TejaDanasvi on 6/10/21.
+//
+
+import Foundation
+class ObserverClass<T> {
+    var value:T?{
+        didSet {
+            observerBlock?(value)
+        }
+    }
+    init(value:T?) {
+        self.value = value
+    }
+    private var observerBlock: ((T?) -> Void)?
+
+    func addObserver(observer:@escaping(T?) -> Void) {
+        self.observerBlock = observer
+    }
+}
+
