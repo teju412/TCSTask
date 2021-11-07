81ee6b54a57b2ba34563f418e4c177f22bcf6517
teja <tejagoud.ios@gmail.com>
2021-11-07 19:55:55 +1100

    added Scene Delegate

--- TableViewTest/SceneDelegate.swift
+++ TableViewTest/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let navController = UINavigationController.init(rootViewController: MainViewController())
+        let navController = UINavigationController.init(rootViewController: NewsListViewController())
         navController.navigationBar.prefersLargeTitles = true
         navController.navigationBar.backgroundColor = .white
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
