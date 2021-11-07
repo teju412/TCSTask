a4ef2cc51beb704b9bce39f477fd82b5f30c22c9
teja <tejagoud.ios@gmail.com>
2021-11-07 19:22:22 +1100

    Added ApiKey Secret

---
+++ TableViewTest/Library/Constants/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  TableViewTest
+//
+//  Created by TejaDanasvi on 7/11/21.
+//
+
+import Foundation
+
+class Secrets {
+    static var newsApiKey = "ebb0fe6c16b8459ab55d8b49569b9f33"
+}
