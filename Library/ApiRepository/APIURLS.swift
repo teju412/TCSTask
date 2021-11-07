8bf812d1fe5b42ffae5389a9df0d354adb8c66b9
teja <tejagoud.ios@gmail.com>
2021-11-07 18:22:53 +1100

    Added api url

---
+++ Library/ApiRepository/APIURLS.swift
@@ -0,0 +1,17 @@
+//
+//  APIURLS.swift
+//  NYTTImesByTeja
+//
+//  Created by TejaDanasvi on 3/10/21.
+//
+
+import Foundation
+
+struct ApiUrls {
+    
+    static var newsApiUrl: String {
+        return "https://newsapi.org/v2/everything"
+    }
+
+}
+
