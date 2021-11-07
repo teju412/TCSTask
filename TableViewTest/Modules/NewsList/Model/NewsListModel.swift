14ef4328151c86e97e6ab1051258c78c159b5989
teja <tejagoud.ios@gmail.com>
2021-11-07 19:57:49 +1100

    Added Model

---
+++ TableViewTest/Modules/NewsList/Model/NewsListModel.swift
@@ -0,0 +1,19 @@
+//
+//  NytSearchModel.swift
+//  ReactivePrograming
+//  Created by TejaDanasvi on 6/10/21.
+//
+
+import Foundation
+
+struct NewsApiResponse: Codable {
+    let status : String?
+    let totalResults: Int?
+    let articles : [NewsApiArticle]?
+}
+
+struct  NewsApiArticle: Codable {
+    let title: String?
+    let description: String?
+    let urlToImage: String?
+}
