2a1a5aee2ced2b10b9ffe816f075c3f7ea41b95e
teja <tejagoud.ios@gmail.com>
2021-11-07 19:58:42 +1100

    Added View Layer

---
+++ TableViewTest/Modules/NewsList/Controller/NewsListViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ListTableViewCell.swift
+//  TableViewTest
+//
+//  Created by TejaDanasvi on 5/11/21.
+//
+
+import UIKit
+class NewsListViewController: UIViewController {
+    
+    // MARK: - Propeties
+    
+    var tableView = UITableView()
+    let refreshControl = UIRefreshControl()
+    
+    // MARK: - Dependency
+    
+    let viewModel: NewsListViewModel
+    
+    // MARK: - Lifecycle
+    
+    init(viewModel: NewsListViewModel = NewsListViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTableView()
+        setupRefresh()
+        self.title = "News Feed"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Setup
+    
+    func setTableView() {
+        tableView.frame = self.view.frame
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = UIColor.clear
+        tableView.estimatedRowHeight = 120
+        self.view.addSubview(tableView)
+        tableView.register(NewsListTableViewCell.self, forCellReuseIdentifier: "Cell")
+        addObservor()
+        viewModel.fetchData {}
+    }
+    
+    func addObservor() {
+        viewModel.newsArticles.addObserver { _ in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - Refresh
+    
+    func setupRefresh() {
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(onPullRefresh), for: .valueChanged)
+    }
+    
+    @objc func onPullRefresh() {
+        self.viewModel.resetPage() {
+            DispatchQueue.main.async {
+                self.refreshControl.endRefreshing()
+            }
+        }
+    }
+}
+
+// MARK: - TableView Delegate
+
+extension NewsListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.newCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? NewsListTableViewCell else {fatalError("Unabel to create cell")}
+        cell.configure(data: (viewModel.newsArticles.value?[indexPath.row])!)
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let contentOffsetY = scrollView.contentOffset.y
+        let scrollViewCanScrollDown = contentOffsetY > scrollView.bounds.height
+        let scrollViewIsAtBottom = (contentOffsetY >= (scrollView.contentSize.height - scrollView.bounds.height - 20))
+        
+        if scrollViewIsAtBottom && scrollViewCanScrollDown {
+            DispatchQueue.main.async {
+                self.viewModel.fetchData {}
+            }
+        }
+    }
+}
---
+++ TableViewTest/Modules/NewsList/Views/NewsListTableViewCell.swift
@@ -0,0 +1,96 @@
+
+
+import UIKit
+
+class NewsListTableViewCell: UITableViewCell {
+    
+    // MARK: - Components
+    
+    lazy var backView: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+    
+    lazy var userImage: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.layer.cornerRadius = CGFloat(40)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    lazy var titleNameLbl: UILabel = {
+        let lbl = UILabel(frame: .zero)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        lbl.font = UIFont.boldSystemFont(ofSize: 18)
+        return lbl
+    }()
+    
+    lazy var descriptionLbl: UILabel = {
+        let lbl = UILabel(frame: .zero)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        lbl.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        return lbl
+    }()
+    
+    // MARK: - Constraints
+    
+    func addBackViewConstraints() {
+        backView.translatesAutoresizingMaskIntoConstraints = false
+        backView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: CGFloat(0)).isActive = true
+        backView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: CGFloat(0)).isActive = true
+        backView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: CGFloat(0)).isActive = true
+        backView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: CGFloat(0)).isActive = true
+    }
+    
+    func addUIImageViewConstraints() {
+        userImage.translatesAutoresizingMaskIntoConstraints = false
+        userImage.leadingAnchor.constraint(equalTo: backView.leadingAnchor, constant: CGFloat(20)).isActive = true
+        userImage.topAnchor.constraint(equalTo: backView.topAnchor, constant: CGFloat(20)).isActive = true
+        userImage.bottomAnchor.constraint(greaterThanOrEqualTo: backView.bottomAnchor, constant: CGFloat(-20)).isActive = true
+        userImage.heightAnchor.constraint(equalToConstant: CGFloat(80)).isActive = true
+        userImage.widthAnchor.constraint(equalToConstant: CGFloat(80)).isActive = true
+    }
+    
+    func addTitleNameLabelConstraints() {
+        titleNameLbl.translatesAutoresizingMaskIntoConstraints = false
+        titleNameLbl.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: CGFloat(20)).isActive = true
+        titleNameLbl.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: CGFloat(-20)).isActive = true
+        titleNameLbl.topAnchor.constraint(equalTo: backView.topAnchor, constant: CGFloat(10)).isActive = true
+    }
+    
+    func addDescriptionLabelConstraints() {
+        descriptionLbl.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLbl.leadingAnchor.constraint(equalTo: titleNameLbl.leadingAnchor).isActive = true
+        descriptionLbl.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: CGFloat(-20)).isActive = true
+        descriptionLbl.topAnchor.constraint(equalTo: titleNameLbl.bottomAnchor, constant: CGFloat(10)).isActive = true
+        descriptionLbl.bottomAnchor.constraint(equalTo: backView.bottomAnchor, constant: CGFloat(-20)).isActive = true
+    }
+
+    // MARK: - Lifecycle
+    
+    func configure(data: NewsApiArticle) {
+        self.titleNameLbl.text = data.title
+        self.descriptionLbl.text = data.description
+        self.userImage.load(url: URL(string: data.urlToImage!)!)
+        addViews()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func addViews() {
+        self.contentView.addSubview(backView)
+        backView.addSubview(userImage)
+        backView.addSubview(titleNameLbl)
+        backView.addSubview(descriptionLbl)
+        addBackViewConstraints()
+        addUIImageViewConstraints()
+        addTitleNameLabelConstraints()
+        addDescriptionLabelConstraints()
+    }
+
+}
