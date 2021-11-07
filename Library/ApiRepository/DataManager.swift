ba6427287e29837630e82868bd1e746e659643ef
teja <tejagoud.ios@gmail.com>
2021-11-07 18:26:08 +1100

    Added api repository class

---
+++ Library/ApiRepository/DataManager.swift
@@ -0,0 +1,28 @@
+//
+//  DataManger.swift
+//  AllPracticeConcepts
+//
+//  Created by TejaDanasvi on 2/10/21.
+//
+
+import Foundation
+
+class DataManager {
+    static func getServerData<T: Decodable>(str: String, type: T.Type, completionHandler: @escaping (_ obj:T?, _ error: Error? )  -> ())  {
+        guard let url = URL(string: str) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let data = data,
+                   error == nil {
+                do {
+                    let userResponse = try JSONDecoder().decode(T.self, from: data)
+                    completionHandler(userResponse, nil)
+                } catch let err {
+                    completionHandler(nil, err)
+                }
+            } else {
+                completionHandler(nil, error)
+            }
+        }.resume()
+    }
+}
